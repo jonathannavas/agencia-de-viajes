0097b09e4322b3dbd9327aa9b080e966476170bf
Jonathan <jonathan.navas9@hotmail.com>
2021-03-02 14:58:26 -0500

    Sitio web para una agencia de viajes practica

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-const express = require('express');
+import express from 'express';
+import router  from './routes/index.js';
+import db from './config/db.js';
 
 const app = express();
+
+//conectar la base de datos
+db.authenticate()
+    .then(()=>{
+        console.log('Base de datos conectada');
+    }).catch((err) => {
+        console.log(err);
+    })
 
 //definir puerto
 
 const port = process.env.PORT || 4000;
 
-app.listen(()=>{
-    console.log(`El servidor esta funcionando  en el puerto ${port}`)
-})
+//habilitar pug
+app.set('view engine','pug');
+
+//obtener el año actual
+app.use((req,res,next)=>{
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombreSitio = "Agencia de Viajes";
+    next();
+});
+
+//agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended:true}));
+
+//definir la carpeta publica
+app.use(express.static('public'));
+
+//agregar router
+
+app.use('/',router);
+
+app.listen(port,()=>{
+    console.log(`El servidor esta funcionando  en el puerto ${port}`);
+});
